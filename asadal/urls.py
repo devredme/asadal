c76d970d9d4886e6854d05f83e7cf3a9d6271477
wishket_yg <yonggill@wishket.com>
2018-01-20 19:51:02 +0900

    base

--- asadal/urls.py
+++ asadal/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from asadal.landing.views import IndexView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-]
+    url(r'^', IndexView.as_view())
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
